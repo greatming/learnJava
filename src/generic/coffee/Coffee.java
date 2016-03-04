2ea76c8d8bc6d65a8fd03321bd54263f90dbdab0
baidu <baidu@MacBook-Pro.local>
2016-03-04 18:31:03 +0800

    init

---
+++ src/generic/coffee/Coffee.java
@@ -0,0 +1,8 @@
+package generic.coffee;
+
+/**
+ * Created by baidu on 16/3/4.
+ */
+public class Coffee {
+    private static long counter = 0;
+}
---
+++ src/util/Generator.java
@@ -0,0 +1,9 @@
+package util;
+
+/**
+ * Created by baidu on 16/3/4.
+ */
+public interface Generator<T> {
+
+    T next();
+}
